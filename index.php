f094fc4426044fe9b42480355d611eb754938fa6
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-27 14:43:54 +0100

    Modif

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 ## Exercice 1 Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016)
 
-echo "<br><br>";
+echo date('d/m/Y') . "<br><br>";
 
 
 
@@ -14,41 +14,48 @@ echo "<br><br>";
 
 echo "<br><br>";
 
-
+echo date('d-m-y') . "<br><br>";
 
 ## Exercice 3 Afficher la date courante avec le jour de la semaine et le mois en toutes lettres (ex : mardi 2 août 2016)
 ## Bonus : Le faire en français.
+setlocale(LC_ALL, ['fr', 'fra', 'fr_FR']);
 
-echo "<br><br>";
+$date = getdate();
+
+echo strftime('%A %d %B %Y' ) ."<br><br>";
 
 
 
 ## Exercice 4 Afficher le timestamp du jour.
 ## Afficher le timestamp du mardi 2 août 2016 à 15h00.
 
-echo "<br><br>";
-
+echo time() . "<br><br>";
+echo mktime(15, 0, 0, 8, 2, 2016) . "<br><br>";
 
 
 ##Exercice 5 Afficher le nombre de jours qui séparent la date du jour avec le 16 mai 2016.
+$dateToday = new DateTime('2021-01-27');
+$dateBefore = new DateTime('2016-05-16');
 
-echo "<br><br>";
+$interval = $dateToday->diff($dateBefore);
+echo $interval->y . " ans " . $interval->m . " mois " . $interval->m . " jours" . "<br><br>";
 
 
 
 ##Exercice 6 Afficher le nombre de jours dans le mois de février de l'année 2016.
-
-echo "<br><br>";
+$dateFebruary = date('t', mktime(0,0,0,2,1, 2016));
+echo $dateFebruary . "<br><br>";
 
 
 
 ##Exercice 7 Afficher la date du jour + 20 jours.
 
-echo "<br><br>";
+$date = date('Y-m-d', strtotime('+20 day'));
+echo $date . "<br><br>";
 
 ##Exercice 8 Afficher la date du jour - 22 jours
-
-echo "<br><br>";
+$date = date('Y-m-d',strtotime('-22 day'));
+echo $date . "<br><br>";
 
 
 ## TP Faire un formulaire avec deux listes déroulantes.
@@ -56,3 +63,25 @@ echo "<br><br>";
 # MEGA BONUS ( surligné et caractère gras :-)  ) : En fonction des choix, afficher un calendrier ( voir libs JS ou autres )
 
 
+echo '<SELECT name="d" Size="1">';
+
+ for($d=1; $d<=12;$d++){
+     if ($d < 10){
+         echo "<OPTION>0$d<br></OPTION>";
+     }
+     else {
+         echo "<OPTION>$d<br></OPTION>";
+     }
+ }
+
+echo "</SELECT>";
+
+$date = date('Y');
+
+echo '<SELECT name="y" Size="1">';
+
+ for ($y=2000; $y<=$date; $y++) {
+     echo "<OPTION><br>$y<br></OPTION>";
+ }
+
+echo "</SELECT>";
